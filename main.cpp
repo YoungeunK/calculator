631798b62a6f650ccab5f87b296ad8ad5705d77c
Yujeong <yujeong.choi.0606@gmail.com>
2019-01-25 17:25:50 +0900

    Main modified

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include<stdio.h>
 #include "hello.h"
+
+using namespace std;
+
+void calculator (void) {
+	int operator_;
+	int a;
+	int b;
+	printf("Press operator that you want to execute\n"); 
+	printf(" 1. Add \n 2. Multiply \n 3. Divide \n");
+	scanf("%d", &operator_);
+	printf("\n\nType first operand\n");
+	scanf("%d", &a);
+	printf("\n\nType second operand\n");
+	scanf("%d", &b);
+
+	if (operator_ == 1)
+		return;
+	else if (operator_ == 2)
+		return;
+	else if (operator_ == 3)
+		return;
+	
+}
+
+
 int main(void)
 {
 	printf("Calculator!\n");
+	calculator();
 	hello();
 }
